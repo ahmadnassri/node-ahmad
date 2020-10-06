a9333e6a67370764f0fb2e220891dad946df9e4d
Ahmad Nassri <ahmad@ahmadnassri.com>
2020-10-06 14:53:15 -0400

    fix(test): update test scripts

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-const ahmad = require('.')
+const ahmad = require('..')
 const { test } = require('tap')
 
 test('imports successfully', assert => {
   assert.plan(1)
 
-  assert.equal(ahmad.name, { first: 'Ahmad', last: 'Nassri' })
+  assert.equal(ahmad.name, 'Ahmad Nassri')
 })
